c8cd2286a6c3462d9e0c9fdc738004c3279ec2aa
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2023-04-19 19:11:59 +0100

    Tasklist initial setup and tweak to prevent error with stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  stories: ["../src/components/**/*.stories.js"],
+  stories: ["../src/components/**/*.stories.@(js|jsx)"],
   staticDirs: ["../public"],
   addons: [
     "@storybook/addon-links",
